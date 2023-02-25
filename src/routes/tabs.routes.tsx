631062d3e377361458e9aad305dc0c6cbed7a84f
jonasbfranco <jonasbfranco@hotmail.com>
2023-02-25 09:36:27 -0300

    :tada: tabroutes=Platform

--- src/routes/tabs.routes.tsx
+++ src/routes/tabs.routes.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import colors from '../styles/colors';
@@ -19,7 +20,7 @@ const AuthRoutes = () => {
                 // inactiveTintColor: colors.heading,
                 // labelPosition: 'beside-icon',
                 tabBarStyle: {
-                    // paddingVertical: 10,
+                    paddingVertical: Platform.OS === 'ios' ? 20 : 0,
                     height: 60,
                 }
                 
